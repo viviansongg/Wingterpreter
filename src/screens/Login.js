dcdc0f87fea32822cbb09824ef63bc77d335c9fc
Vivian Song <viviann.song006@gmail.com>
2025-01-18 21:44:18 -0500

    centered login screen elements

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -72,7 +72,6 @@ function Login() {
         </div>
         <div className='container'>
             <div className='header'>
-                <div className='text'>Login</div>
                 <div className='underline'></div>
             </div>
             <div className='inputs'>
