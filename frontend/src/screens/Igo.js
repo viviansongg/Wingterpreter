feac1e46ff7bff25a64542f25bb790c56b54a2ed
wanga102 <anniew.wang@mail.utoronto.ca>
2025-01-19 04:56:32 -0500

    igo screen

---
+++ frontend/src/screens/Igo.js
@@ -0,0 +1,22 @@
+import {useNavigate} from 'react-router-dom';
+import React, {useState} from 'react';
+
+function Igo() {
+
+    const navigate = useNavigate();
+
+    const handleClickPlayAgain = (e) => {
+        navigate('/mode');
+    }
+
+    return (
+        <>
+        {/* <div className='plainbg'> </div> */}
+        <div className='bg2'> </div>
+            <p className='title' style={{color: 'white', marginTop: '-20px'}}> game over </p>
+            <button className='button-div' style={{marginLeft: '70px'}} onClick={handleClickPlayAgain}> play again </button>
+        </>
+    );
+}
+
+export default Igo;
